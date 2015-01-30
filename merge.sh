b016297055d74ab7cd03ac213aae8505539fbc2a
lonewolf <nikhilkalige@gmail.com>
2015-01-29 20:09:51 -0700

    merge merge script from master

---
+++ merge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+message="$1"
+size=${#message}
+
+if [ -z "$message" ] || [ $size -lt 6 ]; then
+    echo "Enter valid message"
+    exit 1
+fi
+
+git checkout master
+git stash clear
+git add output
+git stash
+# reset to clean
+git reset --hard
+# remove untracked files
+git clean -fd
+# git checkout develop output/
+# git mv -f output/* .
+git stash pop
+rsync -a output/ .
+rm -r output
+git add -A
+git commit -m "$message"
