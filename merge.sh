f67cdf2def4ed9a945f671c820b42b802e085917
lonewolf <nikhilkalige@gmail.com>
2015-01-29 20:08:10 -0700

    use stash kind of method

--- merge.sh
+++ merge.sh
@@ -9,10 +9,15 @@ if [ -z "$message" ] || [ $size -lt 6 ]; then
 fi
 
 git checkout master
+git stash clear
+git add output
+git stash
 # reset to clean
 git reset --hard
 # remove untracked files
 git clean -fd
-git checkout develop output/
-git mv -f output/* .
+# git checkout develop output/
+# git mv -f output/* .
+git stash pop
+git add -A
 git commit -m "$message"
